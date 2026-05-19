bcf063eb1d425fdc8e86a9afb6412b0b87649230
Lacketronik <faliqdoescoding@gmail.com>
2026-05-19 20:16:00 +0800

    Configured Information Services

--- MyResumeBackend/MyResumeBackend/DTOs/InformationDTO.cs
+++ MyResumeBackend/MyResumeBackend/DTOs/InformationDTO.cs
@@ -1,11 +1,56 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace MyResumeBackend.DTOs
 {
     public class InformationDTO
     {
         public string name { get; set; }
         public string linkedin { get; set; }
-        public string[] introduction {  get; set; }
-        public string[] role { get; set; }
         public string? resumeFileID { get; set; }
+
+        [JsonIgnore]
+        public string rawIntroduction { get; set; }
+
+        [JsonIgnore]
+        public string rawRole { get; set; }
+
+        public string[] introduction
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(rawIntroduction)) return Array.Empty<string>();
+                using var doc = JsonDocument.Parse(rawIntroduction);
+                var list = new List<string>();
+                foreach (var element in doc.RootElement.EnumerateArray())
+                {
+                    if (element.TryGetProperty("description", out var prop))
+                    {
+                        list.Add(prop.GetString());
+                    }
+                }
+                return list.ToArray();
+            }
+        }
+
+        public string[] role
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(rawRole)) return Array.Empty<string>();
+
+                using var doc = JsonDocument.Parse(rawRole);
+                var list = new List<string>();
+                foreach (var element in doc.RootElement.EnumerateArray())
+                {
+                    if (element.TryGetProperty("role_name", out var prop))
+                    {
+                        list.Add(prop.GetString());
+                    }
+                }
+                return list.ToArray();
+            }
+        }
     }
 }
---
+++ MyResumeBackend/MyResumeBackend/Services/IInformationService.cs
@@ -0,0 +1,9 @@
+﻿using MyResumeBackend.DTOs;
+
+namespace MyResumeBackend.Services
+{
+    public interface IInformationService
+    {
+        Task<InformationDTO> GetInformation();
+    }
+}
