0b9decee66ec3612d819e8291ad0e61d4717d370
Lacketronik <faliqdoescoding@gmail.com>
2026-07-19 02:04:01 +0800

    removed Unity testing

--- MyResumeWeb/tests/test-unity-demo.spec.ts
+++ MyResumeWeb/tests/test-unity-demo.spec.ts
@@ -2,25 +2,25 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Unity Demo Interaction', () => {
 
-  test.beforeEach(async ({ page }) => {
-    await page.goto('https://localhost:5173/');
-  });
+  // test.beforeEach(async ({ page }) => {
+  //   await page.goto('https://localhost:5173/');
+  // });
 
-  test('Test Unity Demo Interaction', async ({ page }) => {
-    await page.getByRole('button', { name: 'Smart AI Steering Car -' }).click();
-    await page.getByRole('button', { name: 'Play Demo' }).click();
+  // test('Test Unity Demo Interaction', async ({ page }) => {
+  //   await page.getByRole('button', { name: 'Smart AI Steering Car -' }).click();
+  //   await page.getByRole('button', { name: 'Play Demo' }).click();
 
-    const gameFrame = page.frameLocator('iframe[title="Project Demo 1"]')
-                          .frameLocator('#game_drop');
+  //   const gameFrame = page.frameLocator('iframe[title="Project Demo 1"]')
+  //                         .frameLocator('#game_drop');
     
-    const canvas = gameFrame.locator('#unity-canvas');
+  //   const canvas = gameFrame.locator('#unity-canvas');
 
-    await expect(canvas).toBeVisible();
+  //   await expect(canvas).toBeVisible();
 
-    await canvas.click({ position: { x: 521, y: 295 } });
-    await canvas.press('Escape');
+  //   await canvas.click({ position: { x: 521, y: 295 } });
+  //   await canvas.press('Escape');
 
-    await page.getByRole('button', { name: '✕ Close Demo' }).click();
-    console.log('Unity demo interaction and close verified.');
-  });
+  //   await page.getByRole('button', { name: '✕ Close Demo' }).click();
+  //   console.log('Unity demo interaction and close verified.');
+  // });
 });
